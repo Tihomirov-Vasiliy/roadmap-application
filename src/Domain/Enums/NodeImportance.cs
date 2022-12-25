55fa4140b3d6c101e48dc1ece79c2956897e26c2
Tihomirov-Vasiliy <tihomirov.vasili@bk.ru>
2022-12-25 18:47:03 +0600

    create basic console appcreate domain class library with domain classes

---
+++ src/Domain/Enums/NodeImportance.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Enums
+{
+    public enum NodeImportance
+    {
+        None = 0,
+        MustToKnow = 1,
+        GoodToKnow = 2,
+        Maybe = 3
+    }
+}
---
+++ src/Domain/Enums/SourceType.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Enums
+{
+    public enum SourceType
+    {
+        Article = 0,
+        Video = 1,
+        Course = 2,
+        Tool = 3
+    }
+}
